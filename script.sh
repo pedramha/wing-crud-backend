a0e4bec2038ff3f72603ec346f7d752f85b6156b
Pedram Hamidehkhan <pedram.hamidehkhan@gmail.com>
2025-02-20 18:07:58 +0100

    Remove outdated main.yml and __main.w files; add new deploy.yml for AWS deployment

--- script.sh
+++ script.sh
@@ -17,6 +17,5 @@ aws s3api put-bucket-versioning --bucket $bucket_name --versioning-configuration
 # dynamodb for creating lock file, to prevent users simultaneously modifying the state file
 aws dynamodb create-table --table-name $table_name --attribute-definitions AttributeName=LockID,AttributeType=S --key-schema AttributeName=LockID,KeyType=HASH --billing-mode PAY_PER_REQUEST
 echo TF_BACKEND_BUCKET=$bucket_name > .env
-echo TF_BACKEND_KEY=terraform.tfstate >> .env
 echo TF_BACKEND_TABLE=$table_name >> .env
 echo TF_BACKEND_REGION=$region >> .env
